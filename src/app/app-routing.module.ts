c839be80dfb767f4b32fb17884e3ba0c1adcdb76
morgenstern28 <mcrivelente@itelinfo.com>
2022-01-03 10:16:21 -0400

    Adicionando Listar Raca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { AnimalincluirComponent } from './animalincluir/animalincluir.component'
 import { AnimalListarComponent } from './animal-listar/animal-listar.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RacaListarComponent } from './raca-listar/raca-listar.component';
 
 const routes: Routes = [
 
   {path:"animal", component: AnimalListarComponent},
-  {path:"animal/incluir", component: AnimalincluirComponent}
-
+  {path:"animal/incluir", component: AnimalincluirComponent},
+  {path: "raca", component: RacaListarComponent}
 
 ];
 
