1e4b5a8eb315be940e4c5d7e3ac7b5a20522fcb6
henriquelopes42 <lsiqueira@itelinfo.com>
2022-01-03 11:33:11 -0400

    Implementado componente de leilao Incluir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,34 +7,18 @@ import { AnimalListarComponent } from './animal-listar/animal-listar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AnimalincluirComponent } from './animalincluir/animalincluir.component'
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { CompradorListarComponent } from './comprador-listar/comprador-listar.component';
-import { VendedorIncluirComponent } from './vendedor-incluir/vendedor-incluir.component';
-=======
-=======
->>>>>>> 5ed6cd1a96b027be7a72b6593527482876489dbb
 import { RacaListarComponent } from './raca-listar/raca-listar.component';
 import { LeilaoListarComponent } from './leilao-listar/leilao-listar.component';
 import { CompradorListarComponent } from './comprador-listar/comprador-listar.component';
-import { LeilaoincluirComponent } from './leilaoincluir/leilaoincluir.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimalListarComponent,
     AnimalincluirComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    CompradorListarComponent,
-VendedorIncluirComponent
-=======
-=======
->>>>>>> 5ed6cd1a96b027be7a72b6593527482876489dbb
     RacaListarComponent,
     LeilaoListarComponent,
-    CompradorListarComponent,
-    LeilaoincluirComponent
+    CompradorListarComponent
   ],
   imports: [
     BrowserModule,
