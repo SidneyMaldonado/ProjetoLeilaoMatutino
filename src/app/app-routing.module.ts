7e1b92f58748397356733075b46c4553589d1ccb
henriquelopes42 <lsiqueira@itelinfo.com>
2022-01-04 11:23:57 -0400

    Implementado incluir de leilao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { VendedorListarComponent } from './vendedor-listar/vendedor-listar.compo
 import { VendedorIncluirComponent } from './vendedorincluir/vendedorincluir.component';
 import { CompradorListarComponent } from './comprador-listar/comprador-listar.component';
 import { NgModule } from '@angular/core';
+import { MedicoincluirComponent } from './medicoincluir/medicoincluir.component';
 
 
 const routes: Routes = [
@@ -21,7 +22,8 @@ const routes: Routes = [
   {path: "vendedor", component: VendedorListarComponent},
   {path: "medico", component: MedicoListarComponent},
   {path: "vendedor/incluir", component: VendedorIncluirComponent},
-  {path: "leilao/incluir", component: LeilaoincluirComponent}
+  {path: "leilao/incluir", component: LeilaoincluirComponent},
+  {path: "medico/incluir", component: MedicoincluirComponent}
 
 
 ];
