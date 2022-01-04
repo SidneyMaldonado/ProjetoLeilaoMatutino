3718a0a15d6970531a5fbdcd109abd004b90a053
puleu <puleoti@outlook.com>
2022-01-04 07:32:30 -0400

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimalListarComponent } from './animal-listar/animal-listar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AnimalincluirComponent } from './animalincluir/animalincluir.component'
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { CompradorListarComponent } from './comprador-listar/comprador-listar.component';
 import { VendedorIncluirComponent } from './vendedor-incluir/vendedor-incluir.component';
-=======
-=======
->>>>>>> 5ed6cd1a96b027be7a72b6593527482876489dbb
 import { RacaListarComponent } from './raca-listar/raca-listar.component';
 import { LeilaoListarComponent } from './leilao-listar/leilao-listar.component';
-import { CompradorListarComponent } from './comprador-listar/comprador-listar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimalListarComponent,
     AnimalincluirComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     CompradorListarComponent,
-VendedorIncluirComponent
-=======
-=======
->>>>>>> 5ed6cd1a96b027be7a72b6593527482876489dbb
+VendedorIncluirComponent,
     RacaListarComponent,
     LeilaoListarComponent,
     CompradorListarComponent
