233860943a0656698c420d5226497a1f3f1619df
morgenstern28 <mcrivelente@itelinfo.com>
2022-01-04 09:58:35 -0400

    Incluindo listar medico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { CompradorListarComponent } from './comprador-listar/comprador-listar.component';
 import { AnimalincluirComponent } from './animalincluir/animalincluir.component';
 import { AnimalListarComponent } from './animal-listar/animal-listar.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RacaListarComponent } from './raca-listar/raca-listar.component';
 import { LeilaoListarComponent } from './leilao-listar/leilao-listar.component';
+import { MedicoListarComponent } from './medico-listar/medico-listar.component';
 import { VendedorListarComponent } from './vendedor-listar/vendedor-listar.component';
 
 const routes: Routes = [
 
   {path:"animal", component: AnimalListarComponent},
   {path:"animal/incluir", component: AnimalincluirComponent},
-  {path:"comprador", component: CompradorListarComponent},
-  {path: "raca-listar", component: RacaListarComponent},
-  {path: "leilao-listar", component: LeilaoListarComponent},
-  {path: "vendedor", component : VendedorListarComponent}
+  {path: "raca", component: RacaListarComponent},
+  {path: "leilao", component: LeilaoListarComponent},
+  {path: "vendedor", component: VendedorListarComponent},
+  {path: "medico", component: MedicoListarComponent}
 
 ];
 
