016d01758de73543a8b359f213b48abf9b3103bc
AndreAraujo1998 <aflorencio@itelinfo.com>
2022-01-05 11:03:35 -0400

    Mudancas

--- src/app/leilaoalterar/leilaoalterar.component.ts
+++ src/app/leilaoalterar/leilaoalterar.component.ts
@@ -1,4 +1,9 @@
+import { LeilaoService } from './../services/leilao.service';
 import { Component, OnInit } from '@angular/core';
+import { Leilao } from '../entities/leilao';
+import { Mensagem } from '../entities/mensagem';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-leilaoalterar',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeilaoalterarComponent implements OnInit {
 
-  constructor() { }
+  leiloes : Leilao[] = []
+  leilaoService: any;
+  constructor(private servicoLeilao : LeilaoService, private route: ActivatedRoute,
+    private router: Router) { }
 
-  ngOnInit(): void {
+  mensagem: Mensagem = {
+    mensagem: '',
+    erros: []
   }
 
+  leilao: Leilao = {
+    idLeilao: 0,
+    nome: '',
+    descricao: '',
+    data: new Date(),
+    ativo: true
+  }
+  ngOnInit(): void {
+    this.servicoLeilao.listar().subscribe(
+      dados => this.servicoLeilao = dados
+    )
+  }
+  alterar( frm: NgForm){
+    this.leilaoService.alterar(this.leilao).subscribe(
+      alert(this.mensagem.mensagem),
+      console.log(this.mensagem),
+      if (this.mensagem.erros != null){
+        this.mensagem.erros.forEach( (obj) => { alert(obj) })
+      } else {
+        this.router.navigateByUrl("animal")
+      }
+    )
+  }
 }
