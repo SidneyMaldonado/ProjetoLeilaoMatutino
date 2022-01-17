1d5233d7810f16cdd84482f19f863b256ae76683
henriquelopes42 <lsiqueira@itelinfo.com>
2022-01-17 07:07:47 -0400

    Implementado testes para comprador excluir

--- src/app/compradorexcluir/compradorexcluir.component.spec.ts
+++ src/app/compradorexcluir/compradorexcluir.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CompradorService } from './../services/comprador.service';
+import { NgForm } from '@angular/forms';
 
 import { CompradorexcluirComponent } from './compradorexcluir.component';
 
@@ -8,7 +12,9 @@ describe('CompradorexcluirComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CompradorexcluirComponent ]
+      declarations: [ CompradorexcluirComponent, NgForm ],
+      imports:[HttpClientTestingModule, RouterTestingModule],
+      providers:[CompradorService]
     })
     .compileComponents();
   });
@@ -19,7 +25,19 @@ describe('CompradorexcluirComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('O componente precisa ser criado.', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deve renderizar html.', () =>{
+    const expected: string = 'Excluir Comprador';
+    const result = fixture.debugElement.nativeElement.querySelector('#title');
+    expect(result.innerHTML).toBe(expected)
+  });
+
+  it('Deve renderizar o Botão excluir.', () => {
+    const expected: string = 'Excluir';
+    const result = fixture.debugElement.nativeElement.querySelector('#excluir');
+    expect(result.innerHTML).toBe(expected)
+  })
 });
