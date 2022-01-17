dedfe6720cd66b92c1012db12bce36d86d9ea788
puleu <puleoti@outlook.com>
2022-01-17 08:54:14 -0400

    medicoservice

--- src/app/services/medico.service.spec.ts
+++ src/app/services/medico.service.spec.ts
@@ -156,9 +156,7 @@ describe('MedicoService', () => {
 
       const testRequest = httpTestingController.expectOne('http://localhost:8080/medico/2');
       expect(testRequest.request.method).toBe('DELETE');
-      expect(testRequest.request.body.nome).toBe('Ricardo Serafim 1000')
-      console.log('Mensagem: ')
-      console.log(testRequest.request.body)
+      expect(testRequest.request.responseType).toBe('json');
       testRequest.flush(msg);
 
     });
