27a1bc4e03b2a572c19a3dddfbcce069eeee6862
AndreAraujo1998 <aflorencio@itelinfo.com>
2022-01-17 07:46:05 -0400

    Leilao incluir

--- src/app/leilaoincluir/leilaoincluir.component.spec.ts
+++ src/app/leilaoincluir/leilaoincluir.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LeilaoService } from '../services/leilao.service';
 
 import { LeilaoincluirComponent } from './leilaoincluir.component';
 
@@ -8,7 +12,9 @@ describe('LeilaoincluirComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LeilaoincluirComponent ]
+      declarations: [ LeilaoincluirComponent, NgForm ],
+      imports:[HttpClientTestingModule, RouterTestingModule],
+      providers: [LeilaoService]
     })
     .compileComponents();
   });
