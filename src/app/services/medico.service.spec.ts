5c209e10bd623f024c613df7028eaf6a0b93cc76
puleu <puleoti@outlook.com>
2022-01-17 08:50:10 -0400

    a

--- src/app/services/medico.service.spec.ts
+++ src/app/services/medico.service.spec.ts
@@ -156,7 +156,7 @@ describe('MedicoService', () => {
   
       const testRequest = httpTestingController.expectOne('http://localhost:8080/medico/2');
       expect(testRequest.request.method).toBe('DELETE');
-      expect(testRequest.request.body.nome.idMedico).toBe('Ricardo Serafim 1000')
+      expect(testRequest.request.body.nome).toBe('Ricardo Serafim 1000')
       console.log('Mensagem: ')
       console.log(testRequest.request.body)
       testRequest.flush(msg);
